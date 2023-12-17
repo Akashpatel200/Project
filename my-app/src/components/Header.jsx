4302d5b166a3956d16ff0601e60f47c90916be70
Akashpatel2001 <akp18129@gmail.com>
2023-12-17 22:49:10 +0530

    Added instagram link

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -3,8 +3,10 @@
 import React from 'react';
 import './Header.css';
 import profileImage from '../assets/pic.jpg';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 const Header = () => {
+    const instagramLink = 'https://www.instagram.com/cool_akashpatel';
   return (
     <header className="header">
       <nav className="navbar">
@@ -14,6 +16,11 @@ const Header = () => {
           </div>
           <div className="logo">Akash Patel</div>
           <ul className="nav-links">
+          <li>
+              <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} className="instagram-icon" />
+              </a>
+            </li>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
